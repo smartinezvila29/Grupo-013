050c955a70438fa9227a2ea24df97e32287368c3
Agustin E. Lopez Duamante <alopezduamante@gmail.com>
2021-05-22 20:51:48 -0300

    Pagina de inicio

--- trabajoPractico/src/main/java/com/unla/trabajoPractico/controllers/HomeController.java
+++ trabajoPractico/src/main/java/com/unla/trabajoPractico/controllers/HomeController.java
@@ -1,8 +1,20 @@
 package com.unla.trabajoPractico.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.unla.trabajoPractico.helpers.ViewRouteHelper;
+
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
+	
+	@GetMapping("")
+	public String index() {
+		return ViewRouteHelper.INDEX;
+	}
+	
 
 }
